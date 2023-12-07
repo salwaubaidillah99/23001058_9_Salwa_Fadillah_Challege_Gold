a714070dcbfce7b7bdb14b9e6cb3a01323b64696
salwafadillah <salwa.fadillah@iconpln.co.id>
2023-12-07 13:07:03 +0700

    Add Server And Database

---
+++ database/dbUser.js
@@ -0,0 +1,18 @@
+let user = [
+  {
+    id: 1,
+    username: "salwa fadillah1",
+    email: "salwafadillah3@gmail.com",
+  },
+  {
+    id: 2,
+    username: "fadli Hamzah",
+    email: "fadliHamzah2@gmail.com",
+  },
+  {
+    id: 3,
+    username: "Fita Aryadi",
+    email: "FitaHaryadi@gmail.com",
+  },
+];
+module.exports = { user };
---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+// app.use(express.json());
+
+app.listen(PORT, () =>
+  console.log("server running at http://localhost:", PORT)
+);
